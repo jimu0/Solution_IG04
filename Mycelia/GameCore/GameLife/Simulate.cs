af6bf6fe1e8c3f7514e2e46041773ab5d7cecb47
jimu0 <gtdty90@126.com>
2026-04-01 19:00:35 +0800

    fix

--- Mycelia/GameCore/GameLife/Simulate.cs
+++ Mycelia/GameCore/GameLife/Simulate.cs
@@ -34,28 +34,24 @@ internal static class Simulate
     {
         _input = input;
         stepped = false;
-
+        
+        //从真实世界采样一次时间
+        WTime.Sampling();
         // 每帧采样一次，然后从累加器中取出固定数量的步骤进行处理。
-        WTime.Sampling();//从真实世界采样一次时间
         if (WTime.ShouldStep(WTime.fixedDt))
         {
             RunPhase(SimPhase.PreStep);
             while (WTime.ShouldStep(WTime.fixedDt))
             {
                 WTime.ConsumeStep(WTime.fixedDt);//消耗一次固定步时间
-                Step();
+                RunPhase(SimPhase.Step);
+                _state.Advance();
+                _currentTick++;
                 stepped = true;
             }
             RunPhase(SimPhase.PostStep);
         }
         state = _state;
-    }
-
-    private static void Step()
-    {
-        RunPhase(SimPhase.Step);
-        _state.Advance();
-        _currentTick++;
     }
 
     private static void RunPhase(SimPhase phase)
